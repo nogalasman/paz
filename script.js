9a82ef2c692c324ec4aee90fb0024a5373b409c2
Nogalasman <noga.lasman@gmail.com>
2020-11-24 22:10:50 +0200

    add slideshow

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 function sendForm() {
     document.getElementById("contact-form").className = "was-submitted";
 }
+
+function changeImage() {
+    var img = document.getElementById("slide-img");
+
+    img.src = images[x];
+    x++;
+    if (x >= images.length) {
+        x = 0;
+    }
+    setTimeout("changeImage()", 5000);
+}
+
+var images = [],
+    x = 0;
+images[0] = "images/250X110571325734.jpg";
+images[1] = "images/banner_atar.jpg";
+images[2] = "images/paz_250x110103131473.jpg";
+images[3] = "images/250X110668568400.jpg";
+setTimeout("changeImage()", 0);
